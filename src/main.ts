020308be8372996f849c0815a72d4bded6ad9b7c
Tomas E. Crespo <37670842+enritecnolol@users.noreply.github.com>
2022-09-20 22:08:14 -0400

    response interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformationInterceptor } from './core/interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new TransformationInterceptor());
   app.useGlobalPipes(
     new ValidationPipe({
       enableDebugMessages: true,
