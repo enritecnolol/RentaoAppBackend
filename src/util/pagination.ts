117981c0c915da4512fc5f5042bf3e946b5e6657
Tomas E. Crespo <37670842+enritecnolol@users.noreply.github.com>
2022-10-25 21:56:29 -0400

    add totalPages to pagination

--- src/util/pagination.ts
+++ src/util/pagination.ts
@@ -2,14 +2,18 @@ export function paginate(data: any, limit: number, page: number) {
   const dataPaginated = data.slice((page - 1) * limit, page * limit);
   return {
     data: dataPaginated,
-    totalItems: dataPaginated.length,
+    totalItems: data.length,
+    totalPages: Math.ceil(data.length / limit),
   };
 }
+
 export type Query = {
   limit?: number;
   page?: number;
 };
+
 export type DataPaginate = {
   data: [];
   totalItems: number;
+  totalPages: number;
 };
